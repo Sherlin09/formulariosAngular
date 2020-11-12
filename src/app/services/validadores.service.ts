dea1b088d980e3f2ab053da4837702cbd138fd2c
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2020-11-12 10:24:05 -0600

    207 passwords

--- src/app/services/validadores.service.ts
+++ src/app/services/validadores.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +10,29 @@ export class ValidadoresService {
 
   noHerrera( control: FormControl ): {[s:string]: boolean} {
 
-if ( control.value?.toLovwerCase() === 'herrera' ) {
+if ( control.value?.toLowerCase() === 'herrera' ) {
     return {
       noHerrera: true
     }
      
   }
   return null;
+}
+
+passwordsIguales( pass1Name: string, pass2Name: string ){
+
+  return ( formGroup: FormGroup ) => {
+
+    const pass1Control = formGroup.controls[pass1Name];
+    const pass2Control = formGroup.controls[pass2Name];
+
+    if ( pass1Control.value === pass2Control.value ){
+      pass2Control.setErrors(null);
+    } else {
+      pass2Control.setErrors({noEsIgual: true});
+    }
+  }
+
 }
 
 }
